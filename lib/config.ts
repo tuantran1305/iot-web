4e8750088deff57950e3faf180d33bb001071162
tuantran0707 <tuan.tran@nube-io.com>
2025-10-17 19:09:14 +0700

    fix server and device-id

--- lib/config.ts
+++ lib/config.ts
@@ -1,9 +1,9 @@
 const config = {
   // tbServer: "thingsboard.cloud",
   // tbServer: "212.38.94.144:8081",
-  tbServer: "212.38.94.144",
+  tbServer: "72.60.209.148",
   // tbServer: "thingsboard.srv524038.hstgr.cloud",
-  deviceId: "adde35b0-21d0-11ef-a200-57fe9b2f04fd",
+  deviceId: "034a74f0-ab50-11f0-8b25-c348808b6e0d",
 };
 
 export { config };
